96b6842fc2a6e26f8cfec46f7a097ea9e297c756
Pothireddy Gelivi <pothireddy@example.com>
2026-02-11 12:48:08 +0530

    fix error

--- InfinityMart/urls.py
+++ InfinityMart/urls.py
@@ -23,7 +23,7 @@ import store
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',home,name='home'),
+    path('',home,name='home'),
     path('store/',include('store.urls')),
     path('cart/',include('carts.urls'))
 ] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
